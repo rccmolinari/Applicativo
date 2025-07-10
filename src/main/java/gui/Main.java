6155f9589f7dd4748ab3e3ca2721bb9333ba3237
rccmolinari <rocco0560@gmail.com>
2025-07-10 15:55:25 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Home");
         Login login = new Login();
-        frame.setResizable(true);
+        frame.setResizable(false);
         frame.setContentPane(login.getMainPanel());
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
